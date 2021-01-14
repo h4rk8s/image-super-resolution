da309dceae296b6f9619b5ce3e3153d3170052a1
h4rk8s <63350517+h4rk8s@users.noreply.github.com>
2021-01-14 18:51:05 +0800

    Update rrdn.py

--- ISR/models/rrdn.py
+++ ISR/models/rrdn.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
-tf.config.gpu.set_per_process_memory_fraction(0.5)
-tf.config.gpu.set_per_process_memory_growth(True)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
 
 from tensorflow.keras.initializers import RandomUniform
 from tensorflow.keras.layers import concatenate, Input, Activation, Add, Conv2D, Lambda
